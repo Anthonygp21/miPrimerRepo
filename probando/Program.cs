e72f5aa5fc637f8009a02c59faca13fd8c953256
Anthony Guerrero <Anthonyggp@outlook.es>
2026-06-17 15:02:19 -0500

    intento 3

--- probando/Program.cs
+++ probando/Program.cs
@@ -1,3 +1,3 @@
 ﻿Console.WriteLine("Hello, Platzi!");
 
-string aws_key = "AKIAIOSFODNN7EXAMPLE";
+string stripe_key = "sk_test_51NXe7SJhG8vN3X8Y2zP9K1mQ5rT7vX8zN3W2qL5k8jH4gF3dS2a1pO0i9u8y7t6r5e4w3q2j1HkL";
